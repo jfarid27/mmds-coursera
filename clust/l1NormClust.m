89cb838442002f292e08de0cc9f4cbf169ec0ed7
jfarid27 <jrab227@gmail.com>
2014-10-12 21:35:14 -0400

    Fixed l1 l2 norm clusts.

--- clust/l1NormClust.m
+++ clust/l1NormClust.m
@@ -3,12 +3,12 @@ function clusters = l1NormClust(pointMatrix, centroidMatrix)
 
   for(i = 1:size(pointMatrix)(1))
 
-    winner = Infinity(1, 2);
+    winner = Inf(1, 2);
     winnerDist = Inf;
 
     for (j = 1:size(centroidMatrix)(1))
 
-      distance = l1NormDist(pointMatrix(i, :), centroidMatrix(j, :) < winnerDist;
+      distance = l1NormDist( pointMatrix(i,:) , centroidMatrix(j,:) );
       
       if (distance < winnerDist)
 
--- clust/l2NormClust.m
+++ clust/l2NormClust.m
@@ -3,12 +3,12 @@ function clusters = l2NormClust(pointMatrix, centroidMatrix)
 
   for(i = 1:size(pointMatrix)(1))
 
-    winner = Infinity(1, 2);
+    winner = Inf(1, 2);
     winnerDist = Inf;
 
     for (j = 1:size(centroidMatrix)(1))
 
-      distance = l2NormDist(pointMatrix(i, :), centroidMatrix(j, :) < winnerDist;
+      distance = l2NormDist( pointMatrix(i,:), centroidMatrix(j,:) );
       
       if (distance < winnerDist)
 
